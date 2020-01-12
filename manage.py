44e8c232021c299d4ed0e8a652ec21e25cc55c1c
junedr375 <49837673+junedr375@users.noreply.github.com>
2020-01-12 12:31:31 +0530

    First commit

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+import django_heroku
 
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mac.settings')
@@ -13,9 +13,10 @@ def main():
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
-        ) from exc
+        )
     execute_from_command_line(sys.argv)
 
 
 if __name__ == '__main__':
     main()
+django_heroku.settings(locals())
--- shop/views.py
+++ shop/views.py
@@ -20,6 +20,8 @@ def index(request):
 		allProds.append([prod, range(1, nSlides), nSlides])
 	params = {'allProds':allProds}			
 	return render(request, 'shop/index.html',params)
+
+    
 def match(query,item):
     query=query.lower()
     item.desc=item.desc.lower()
